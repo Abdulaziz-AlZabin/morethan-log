2a35ffaddbc3ea2692edb4a3a1ba28c83cb02e5f
4ΣiΣ <zabinaziz0@gmail.com>
2025-08-22 00:54:26 +0300

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Abdulaziz Al-Zabin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "CyberSecurity Enthusiast",
     bio: "I like watching logs :)",
     email: "zabinaziz0@gmail.com",
